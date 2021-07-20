c044c6c69466dcc40346d626ab34cb4e12c06525
SnapV2 <tawatchaii2007@gmail.com>
2021-07-20 16:48:37 +0700

    ter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import {MapView,ListView} from './Components';  
-import 'leaflet/dist/leaflet.css'
+import { MapView, ListView, Detail } from "./Components";
+import "leaflet/dist/leaflet.css";
 import axios from "axios";
-import React, { useState,useEffect} from 'react';
-
-
+import React, { useState, useEffect } from "react";
 
 function App(props) {
+  const [locationArrey, SetdataMap] = useState([]);
 
-  const [locationArrey,SetdataMap]= useState([]);
-
-useEffect(()=>{
-
-  axios.get("http://localhost:4000/api/get/us-project").then((locationArrey)=>{
-
-    SetdataMap(locationArrey.data)
-  })
-})
-
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/get/us-project")
+      .then((locationArrey) => {
+        SetdataMap(locationArrey.data);
+      });
+  });
 
   return (
     <div className="App">
-   
-    <MapView locationArrey ={locationArrey}/>
-     <ListView/>
+      <Detail />
+      <ListView />
     </div>
   );
 }
 
 export default App;
 
-//   // 
+//   //
--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,42 +1,132 @@
-import './Css/Detail.scss';
-import Button from "@material-ui/core/Button";
-import React from "react";
-import { slide as Menu } from "react-burger-menu";
-import "./Css/listView.scss";
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+/* eslint-disable jsx-a11y/alt-text */
+//import logo from './logo.svg';
+//import React from 'react';
+//import Button from "@material-ui/core/Button";
+import Button from "reactstrap/lib/Button";
+//import Container from "react-bootstrap/Container";
+import { Col, Row } from "reactstrap";
+import "./Css/Detail.css";
 
+function Detail() {
+  return (
+    <div className="App">
+      <div className="page-main content">
+        <Row>
+          <Col>
+            <img src={"icon.png"} height={260} width={250} />
+          </Col>
+        </Row>
+        <Col xs={9}>
+          <h3 className="subtitle is-7">ชุมชนบ้านปราสาท</h3>
+          <p>ตำบลระแงง อำเภอศรีขรภูมิ จังหวัดสุรินทร์</p>
+        </Col>
 
-function Detail () {
-  
-  const [value, setValue] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
- 
-  
-  
+        <Row>
+          <Col>
+            <img src={"."} height={300} width={250} />
+          </Col>
+          <Col xs={9}>
+            <div class="page-leftber content"></div>
+            <div class="button">
+              <Button
+                variant="contained"
+                style={{ margin: "1px 1px 1px 1px", padding: "10px 45px" }}
+              >
+                ความเชี่ยวชาญ
+              </Button>
+              <Button
+                variant="contained"
+                style={{
+                  blackguard: "1px 1px 1px 1px",
+                  padding: "10px 45px",
+                }}
+              >
+                ความรู้
+              </Button>
+              <Button
+                variant="contained"
+                style={{ margin: "1px 1px 1px 1px", padding: "10px 45px" }}
+              >
+                รางวัล
+              </Button>
+              <Button
+                variant="contained"
+                style={{ margin: "1px 1px 1px 1px", padding: "10px 45px" }}
+              >
+                งานวิจัยร่วม
+              </Button>
+              <Button
+                variant="contained"
+                style={{ margin: "1px 1px 1px 1px", padding: "10px 45px" }}
+              >
+                ผู้ประสานงาน
+              </Button>
+            </div>
+          </Col>
+        </Row>
 
-return(
-      <div className = "App">
-     
-        <div className="list-view-menu">
-        <Menu >
-              <Button variant="contained">หน้าแรก</Button>
-              <Button >งานวิจัย</Button>
-              <Button >พื้นที่งานวิจัย</Button>
-              <Button >ข้อมูลนักวิจัย</Button>
-        </Menu>
-        </div> 
-
-      
-
-        
+        {/* /.card */}
       </div>
+
+      <div class="grid">
+        <div class="page-footer content">
+          <h3 className="subtitle is-7"> </h3>
+        </div>
+        <div class="page-leftber content">
+          <img src={"./RMUTI_KORAT.png"} height={120} width={70} />
+          <img src={"./logo3.png"} height={120} width={70} />
+
+          <h3 className="subtitle is-7">หน้าแรก</h3>
+          <h3 className="subtitle is-7">งานวิจัย</h3>
+          <h3 className="subtitle is-7">พื้นที่งานวิจัย</h3>
+          <h3 className="subtitle is-7">ข้อมูลนักวิจัย</h3>
+          <h3 className="subtitle is-7">ข้อมูลนักวิจัย</h3>
+        </div>
+
+        <div class="page-main content">
+          <div>
+            <img src={"."} height={300} width={250} />
+
+            <div class="button">
+              <Button
+                variant="contained"
+                style={{ margin: "1px 1px 1px 1px", padding: "10px 45px" }}
+              >
+                ความเชี่ยวชาญ
+              </Button>
+              <Button
+                variant="contained"
+                style={{
+                  blackguard: "1px 1px 1px 1px",
+                  padding: "10px 45px",
+                }}
+              >
+                ความรู้
+              </Button>
+              <Button
+                variant="contained"
+                style={{ margin: "1px 1px 1px 1px", padding: "10px 45px" }}
+              >
+                รางวัล
+              </Button>
+              <Button
+                variant="contained"
+                style={{ margin: "1px 1px 1px 1px", padding: "10px 45px" }}
+              >
+                งานวิจัยร่วม
+              </Button>
+              <Button
+                variant="contained"
+                style={{ margin: "1px 1px 1px 1px", padding: "10px 45px" }}
+              >
+                ผู้ประสานงาน
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p></p>
+    </div>
   );
 }
-
-
-export default Detail ;
+export default Detail;
